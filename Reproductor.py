f78e665e773ce4e7cf256b922aa0e6bb566ecfcf
María Inés Cardona López <ines7r@gmail.com>
2025-05-03 00:13:35 -0600

    Creación de Clases
    - Clase Nodo
    - Clase ListaReproduccion

---
+++ Reproductor.py
@@ -0,0 +1,52 @@
+class NodoCancion:
+    def __init__(self, nombre, artista, duracion, ruta):
+        self.nombre = nombre
+        self.artista = artista
+        self.duracion = duracion
+        self.ruta = ruta
+        self.anterior = None
+        self.siguiente = None
+
+    def __str__(self):
+        return f"{self.nombre} - {self.artista} ({self.duracion})"
+
+
+class ListaReproduccion:
+    def __init__(self):
+        self.actual = None
+
+    def agregar(self, nodo):
+        if self.actual is None:
+            self.actual = nodo
+            nodo.anterior = nodo.siguiente = nodo
+        else:
+            ultimo = self.actual.anterior
+            ultimo.siguiente = nodo
+            nodo.anterior = ultimo
+            nodo.siguiente = self.actual
+            self.actual.anterior = nodo
+
+    def obtener_actual(self):
+        return self.actual
+
+    def siguiente(self):
+        if self.actual:
+            self.actual = self.actual.siguiente
+        return self.actual
+
+    def anterior(self):
+        if self.actual:
+            self.actual = self.actual.anterior
+        return self.actual
+
+    def obtener_lista(self):
+        canciones = []
+        if not self.actual:
+            return canciones
+        nodo = self.actual
+        while True:
+            canciones.append(nodo)
+            nodo = nodo.siguiente
+            if nodo == self.actual:
+                break
+        return canciones
